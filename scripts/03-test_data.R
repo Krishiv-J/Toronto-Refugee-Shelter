3a522fcdefbfdb6782ac4d4ba9c06ca12a21d4c6
Krishiv-J <krishiv.jain@mail.utoronto.ca>
2024-01-21 13:48:07 -0500

    Tests for Data
    Tests to verify if the data is as expected

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,24 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests to verify data is as expected
+# Author: Krishiv
+# Date: 21 January 2024
+# Contact: krishiv.jain@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: none
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+
+##Check the minimum number is equal, or greater than, 0 ##
+#Based on: https://tellingstorieswithdata.com/02-drinking_from_a_fire_hose.html#simulate
+final_refugees_data$newly_identified |> min() >= 0
+final_refugees_data$total |> min() >= 0
+final_refugees_data$population_group_percentage |> min() >= 0
+
+##Check the percent is not higher than 100 ##
+final_refugees_data$population_group_percentage |> max() <= 100
+
+##Check class of date col is Date ##
+final_refugees_data$date |> class() == "Date"
