9a5c74190d876e8ba87d5de12c7ad2df7f5b7b0f
Krishiv-J <krishiv.jain@mail.utoronto.ca>
2024-01-22 00:26:08 -0500

    Debugging
    Changed the column names due to changes made in data cleaning

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -13,12 +13,12 @@ library(tidyverse)
 
 ##Check the minimum number is equal, or greater than, 0 ##
 #Based on: https://tellingstorieswithdata.com/02-drinking_from_a_fire_hose.html#simulate
-final_refugees_data$newly_identified |> min() >= 0
-final_refugees_data$total |> min() >= 0
-final_refugees_data$population_group_percentage |> min() >= 0
+final_refugees_data$New_Refugees |> min() >= 0
+final_refugees_data$Total_Refugees |> min() >= 0
+final_refugees_data$Percentage_of_Total |> min() >= 0
 
 ##Check the percent is not higher than 100 ##
-final_refugees_data$population_group_percentage |> max() <= 100
+final_refugees_data$Percentage_of_Total |> max() <= 100
 
 ##Check class of date col is Date ##
-final_refugees_data$date |> class() == "Date"
+final_refugees_data$Date |> class() == "Date"
